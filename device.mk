0122af005c1286ca7d443cff2f674a48cc077076
SagarMakhar <sagarmakhar@gmail.com>
2018-10-13 16:26:55 +0000

    X00TD: Add zenmotions
    X00TD: add ambient gestures
    
    X00TD: add gesturesXy
    
    X00TD: fix gestures
    
    hide device settings
    
    remove useless things
    
    another attempt to unify both
    
    zenmotions
    
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    
    remove devicesettings
    
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    
    cleanup

--- device.mk
+++ device.mk
@@ -162,6 +162,10 @@ PRODUCT_PACKAGES += \
     memtrack.sdm660 \
     libgenlock
 
+# Doze
+PRODUCT_PACKAGES += \
+    Zenmotions
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
@@ -258,7 +262,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay native
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@1.0-service-sdm
+    vendor.aosp.livedisplay@1.0-service-sdm
 
 # Low power Whitelist
 PRODUCT_COPY_FILES += \
@@ -331,7 +335,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-aosp
 
 # Power
 PRODUCT_PACKAGES += \
@@ -454,6 +458,10 @@ PRODUCT_PACKAGES += \
 # USB
 PRODUCT_PACKAGES += \
 	android.hardware.usb@1.0-service.basic
+
+# Vendor security patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.aosp.build.vendor_security_patch=2018-08-01
 
 # Vendor properties
 -include $(LOCAL_PATH)/vendor_prop.mk
